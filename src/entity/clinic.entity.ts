3a2f202126c5387a8ed8f1c910281c54a450e193
Andres Zubieta <andres.zubieta@ucb.edu.bo>
2025-05-25 21:50:06 -0400

    Esta si es la creacion de las entidades medico clinica y paciente con codigo correcto

---
+++ src/entity/clinic.entity.ts
@@ -0,0 +1,16 @@
+import { Column,Entity,ForeignKey,PrimaryColumn,PrimaryGeneratedColumn,OneToMany,JoinColumn } from "typeorm";
+import { UUID } from "typeorm/driver/mongodb/bson.typings";
+import { Medico } from "./users.entity";
+
+
+@Entity()
+export class Clinica{
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column()
+    nombre: string
+
+    @OneToMany(() => Medico, (medico) => medico.clinica) // note: we will create author property in the Photo class below
+    medicos: Medico[]
+}
---
+++ src/entity/users.entity.ts
@@ -0,0 +1,96 @@
+import { Column,Entity,ForeignKey,PrimaryColumn,PrimaryGeneratedColumn,ManyToOne } from "typeorm";
+import { UUID } from "typeorm/driver/mongodb/bson.typings";
+import { Clinica } from "./clinic.entity";
+
+
+@Entity()
+export class Paciente{
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+    ////             sdafdsfasdfasdfddslfjasljdslkjlkjjkllkdj
+
+    @Column()
+    ci:number
+
+    @Column()
+    nombre: string
+
+    @Column()
+    apellido: string
+
+    @Column()
+    fechaNac: Date
+    
+    @Column()
+    penalizado: boolean
+
+    @Column()
+    estadoCivil: string
+
+    @Column()
+    direccion: string
+
+    @Column()
+    correoElectronico: string
+
+    @Column()
+    tipoSangre: string
+
+    @Column()
+    telefono: number
+
+    @Column()
+    lugarNac: string
+
+    @Column()
+    genero : string
+
+}
+
+@Entity()
+export class Medico {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column()
+    ci:number
+
+    @Column()
+    nombre: string
+
+    @Column()
+    apellido: string
+
+
+    @ManyToOne(() => Clinica, (clinica) => clinica.medicos)
+    clinica: Clinica
+
+    @Column()
+    fechaNac: Date
+    
+    @Column()
+    penalizado: boolean
+
+    @Column()
+    estadoCivil: string
+
+    @Column()
+    direccion: string
+
+    @Column()
+    correoElectronico: string
+
+    @Column()
+    tipoSangre: string
+
+    @Column()
+    telefono: number
+
+    @Column()
+    lugarNac: string
+
+    @Column()
+    genero : string
+
+
+}
